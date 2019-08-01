cc8cb423884d8cb39421a5e22cec984aed1600eb
Ying <tinying@38f9d3dae8d9.ant.amazon.com>
2019-08-01 21:36:17 +0800

    nonzero, still some bug

---
+++ src/operator/numpy/np_nonzero_op-inl.h
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*!
+ * Copyright (c) 2018 by Contributors
+ * \file boolean_mask-inl.h
+*/
+
+#ifndef MXNET_OPERATOR_NUMPY_NP_NONZERO_OP_INL_H_
+#define MXNET_OPERATOR_NUMPY_NP_NONZERO_OP_INL_H_
+
+#include <dmlc/logging.h>
+#include <dmlc/parameter.h>
+#include <mxnet/operator.h>
+#include <mxnet/ndarray.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include "../operator_common.h"
+#include "../mxnet_op.h"
+#include "../tensor/init_op.h"
+#include "../mshadow_op.h"
+#include "../elemwise_op_common.h"
+
+#define NONZERO_NDIM_SWITCH(NDim, ndim, ...)         \
+  if (NDim == 0) {                                 \
+  } else if (NDim == 1) {                          \
+    const int ndim = 1;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 2) {                          \
+    const int ndim = 2;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 3) {                          \
+    const int ndim = 3;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 4) {                          \
+    const int ndim = 4;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 5) {                          \
+    const int ndim = 5;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 6) {                          \
+    const int ndim = 6;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 7) {                          \
+    const int ndim = 7;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 8) {                          \
+    const int ndim = 8;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 9) {                          \
+    const int ndim = 9;                            \
+    {__VA_ARGS__}                                  \
+  } else if (NDim == 10) {                         \
+    const int ndim = 10;                           \
+    {__VA_ARGS__}                                  \
+  } else {                                         \
+    LOG(FATAL) << "ndim=" << NDim << "too large "; \
+  }
+
+namespace mxnet {
+namespace op {
+
+struct NonzeroForwardKernel {
+  template<int ndim>
+  __global__ static void Map(int i,
+                  int64_t* out,
+                  const int32_t* idx,
+                  const mshadow::Shape<ndim>& shape) {
+    int32_t prev = (i == 0) ? 0 : idx[i - 1];
+    int32_t curr = idx[i];
+    if (prev != curr) {
+      mshadow::Shape<ndim> coord = mxnet_op::unravel<ndim>(i, shape);
+      for(int j=0; j<ndim; j++){
+        out[prev * ndim + j] = coord[j];
+      }
+    }
+  }
+};
+
+} // namespace op
+} // namespace mxnet
+
+#endif
---
+++ src/operator/numpy/np_nonzero_op.cu
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*!
+ * Copyright (c) 2018 by Contributors
+ * \file boolean_mask.cu
+*/
+
+#include "np_nonzero_op-inl.h"
+#include <cub/cub.cuh>
+
+namespace mxnet {
+namespace op {
+
+struct PrefixSumInit{
+  template<typename DTypeIn, typename DTypeOut>
+  __global__ static void Map(int i,
+                  DTypeOut* out,
+                  DTypeIn* in) {
+    if(in[i]){
+    out[i] = DTypeOut(1);
+    }
+  }
+};
+
+#define MAXDIM 10
+
+inline void NonzeroForward(const nnvm::NodeAttrs& attrs,
+                           const OpContext &ctx,
+                           const std::vector<NDArray> &inputs,
+                           const std::vector<OpReqType> &req,
+                           const std::vector<NDArray> &outputs) {
+  using namespace mshadow;
+  CHECK_EQ(inputs.size(), 1U);
+  CHECK_EQ(outputs.size(), 1U);
+  const NDArray &in = inputs[0];
+  const NDArray &out = outputs[0];
+  CHECK_LE(in.shape().ndim(), MAXDIM) << "ndim of input cannot larger than " << MAXDIM;
+  size_t in_size = in.shape().Size();
+  int32_t valid_num = 0;
+  Stream<gpu>* stream = ctx.get_stream<gpu>();
+  int32_t* prefix_sum = nullptr;
+  void* d_temp_storage = nullptr;
+  size_t temp_storage_bytes = 0;
+  // Calculate total temporary memory size
+  cub::DeviceScan::InclusiveSum(d_temp_storage,
+                                temp_storage_bytes,
+                                prefix_sum,
+                                prefix_sum,
+                                in_size,
+                                Stream<gpu>::GetStream(stream));
+  size_t buffer_size = in_size * sizeof(int32_t);
+  temp_storage_bytes += buffer_size;
+  // Allocate memory on GPU and allocate pointer
+  Tensor<gpu, 1, char> workspace =
+    ctx.requested[0].get_space_typed<gpu, 1, char>(Shape1(temp_storage_bytes), stream);
+  prefix_sum = reinterpret_cast<int32_t*>(workspace.dptr_);
+  d_temp_storage = workspace.dptr_ + buffer_size;
+  MSHADOW_TYPE_SWITCH(in.dtype(), IType, {
+    mxnet_op::Kernel<PrefixSumInit, gpu>::Launch(
+      stream, in_size, prefix_sum, in.data().dptr<IType>());
+  });
+  // Calculate prefix sum
+  cub::DeviceScan::InclusiveSum(d_temp_storage,
+                                temp_storage_bytes,
+                                prefix_sum,
+                                prefix_sum,
+                                in_size,
+                                Stream<gpu>::GetStream(stream));
+  CUDA_CALL(cudaMemcpy(&valid_num, &prefix_sum[in_size - 1], sizeof(int32_t),
+                       cudaMemcpyDeviceToHost));
+  // set the output shape forcefully
+  mxnet::TShape s(2, in.shape().ndim());
+  s[0] = valid_num;
+  const_cast<NDArray &>(out).Init(s);
+  // get the shape from the input
+  NONZERO_NDIM_SWITCH(in.shape().ndim(), ndim, {
+    mshadow::Shape<ndim> shape = in.shape().get<ndim>();
+    mxnet_op::Kernel<NonzeroForwardKernel, gpu>::Launch(
+      stream, in_size, out.data().dptr<int64_t>(), prefix_sum, shape);
+  })
+}
+
+NNVM_REGISTER_OP(_npx_nonzero)
+.set_attr<FResourceRequest>("FResourceRequest",
+  [](const NodeAttrs& attrs) {
+    return std::vector<ResourceRequest>{ResourceRequest::kTempSpace};
+  })
+.set_attr<FComputeEx>("FComputeEx<gpu>", NonzeroForward);
+
+} // namespace op
+} // namespace mxnet
